1a3dbb57d1d328373c2bdc3cc9f7114a86ece691
Marques30 <marques.butilla@gmail.com>
2016-02-07 21:38:19 -0800

    Almost done fuckers

--- fill-in-the-blank.py
+++ fill-in-the-blank.py
@@ -1,7 +1,7 @@
 parts_of_speech1 = ["NAME", "PERSON", "VERB", "PLACE","ADJECTIVE"]
 test_string1 = "My name is NAME, I live in a place called PLACE. Everyday I VERB, with my three friends: Justin, Angie, and PERSON."
-test_string2 = ""
-test_string3 = ""
+test_string2 = "For some reason my friends always argue about ADJECTIVE. And everyday I have no idea why. There is another group of people well more of a pair Mark and PERSON. There the kinds of people that VERB and who say they will help then disappear to go to PLACE."
+test_string3 = "However, today is the day that I finally ADJECTIVE. It has finally come to the point where I can leave everyone and go to PLACE. But before any of this I have to meet PERSON. Because he/she is the one to teach me VERB."
 def word_in_pos(word, parts_of_speech):
 	for pos in parts_of_speech:
 		if pos in word:
@@ -22,4 +22,4 @@ def play_game(ml_string, parts_of_speech):
 	replaced = " ".join(replaced)
 	return replaced
 
-print play_game(test_string3,parts_of_speech1)
+print play_game(test_string3, parts_of_speech1)
