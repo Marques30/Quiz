c42da38aa9c86c764e5edcae4c3bd9e158ef6187
Marques30 <marques.butilla@gmail.com>
2016-02-07 20:47:25 -0800

    fuck you guys

---
+++ fill-in-the-blank.py
@@ -0,0 +1,48 @@
+ml_string = "PLACE is really ADJECTIVE"
+parts_of_speech = ["PLACE","ADJECTIVE"]
+def quiz(ml_string,parts_of_speech):
+	replaced[]
+	ml_string = ml_string.split()
+	for word in ml_string
+		replacement = word_pos(word, parts_of_speech)
+		if replacement != None:
+			word = word.replace(replacement,"corg")
+			replaced.append(word)
+		else:
+			replaced.append(word)
+	replaced = "".join(replaced)
+	return replaced
+	
+	print("level 1")
+	print("Once upon a __1__ there was a man named Clark. Clark was someone that was __2__ as a weird child. Barely anyone noticed Clark's hidden secert because unknown to everyone Clark was __3__. But having such a sercert meant that there were many __4__ that Clark had to make.")
+	print()
+	print("fill in the blank for 1")
+	print()
+	print("fill in the blank for 2")
+	print()
+	print("fill in the blank for 3")
+	print()
+	print("fill in the blank for 4")
+	print()
+	print("level 2")
+	print("In a place not that far away from Clark was city known as __1__. There lived another man of mystery, this man preferred to work in __2__. He was the person the people desired but not __3__. Because he struck fear into all those around him, and was soon know as __4__.")
+	print()
+	print("fill in the blank for 1")
+	print()
+	print("fill in the blank for 2")
+	print()
+	print("fill in the blank for 3")
+	print()
+	print("fill in the blank for 4")
+	print()
+	print("level 3")
+	print("However, there is someone who is not only more famous but more cherished than the other two. He is person that everyone respects and be more than willing to give me our __1__. He fights to protect the weak and innocennt and is always looking __2__. Whoever he fights, it doesn't matter how strong they are or what their __3__ are, he will also will. That is why he is not only the __4__ of earth but of the entire universe.")
+	print()
+	print("fill in the blank for 1")
+	print()
+	print("fill in the blank for 2")
+	print()
+	print("fill in the blank for 3")
+	print()
+	print("fill in the blank for 4")
+	print()
