e068c7f1adea654cbd265dd849525d0bc0664fe0
Marques30 <marques.butilla@gmail.com>
2016-02-07 18:49:42 -0800

    corrected

--- beta.py
+++ beta.py
@@ -3,18 +3,6 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 
 def quiz():
-    print("Here is a quiz to test your knowledge of farming...")
-    print()
-    print()
-    print("Question 1")
-    print("What percentage of the land is used for farming?")
-    print()
-    print("a. 25%")
-    print("b. 50%")
-    print("c. 75%")
-    answer = input("Make your choice: ")
-    if answer == "c":
-        print("Correct!")
     print("Here is a quiz to test your general knowledge:")
     print()
     print()
